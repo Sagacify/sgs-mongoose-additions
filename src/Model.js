9deb5c739a6245bd1dcbc26054d0a8cb4e75cad2
François Beuvens <francois.beuvens@gmail.com>
2014-09-24 18:13:43 +0200

    ok

---
+++ src/Model.js
@@ -0,0 +1,27 @@
+var _ = require('underscore');
+	_.str = require('underscore.string');
+	_.mixin(_.str.exports());
+
+var async = require('async')
+
+module.exports = function(mongoose){
+
+	_(mongoose.Model).extend({
+
+		get: function(path, type, options, callback) {
+			var camelizedGetPath = _('get_'+path).camelize();
+			if(typeof this[camelizedGetPath] === 'function'){
+				return this[camelizedGetPath](options, callback);
+			}
+
+			var camelizedPath = _(path).camelize();
+			if(typeof this[camelizedPath] === 'function'){
+				return this[camelizedPath](options, callback);
+			}
+
+			callback('No path ' + path + ' in model');
+		}
+
+	});
+
+};
